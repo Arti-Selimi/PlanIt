eb60ea16a564d7eee3ef3fb288d386e34b3a51be
Arti <selimiarti@outlook.com>
2024-10-30 09:59:40 +0100

    added arrow icon only when page is scrollable

--- app/client-layout.js
+++ app/client-layout.js
@@ -1,16 +1,48 @@
 "use client";
 
 import ArrowDown from "@/components/arrowDown/arrowDown";
+import styles from "./client-layout.module.scss";
+import { useEffect, useState, useRef } from "react";
 
 export default function ClientLayout({ children }) {
+  const [isScrollable, setIsScrollable] = useState(false);
+  const mainRef = useRef(null);
+
+  const checkScrollability = () => {
+    if (mainRef.current) {
+      const { scrollHeight, clientHeight } = mainRef.current;
+      setIsScrollable(scrollHeight > clientHeight);
+      console.log(mainRef.current.scrollTop)
+    }
+  };
+
+  useEffect(() => {
+    checkScrollability();
+    window.addEventListener("resize", checkScrollability);
+    window.addEventListener("popstate", checkScrollability);
+
+    const resizeObserver = new ResizeObserver(checkScrollability);
+    if (mainRef.current) {
+      resizeObserver.observe(mainRef.current);
+    }
+
+    return () => {
+      window.removeEventListener("resize", checkScrollability);
+      window.removeEventListener("popstate", checkScrollability);
+      if (mainRef.current) {
+        resizeObserver.unobserve(mainRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <>
-      <div id="main">
-        <div id="container">
-          {children}
+    <div className={styles.main} id="main" ref={mainRef}>
+      {children}
+      {isScrollable && (
+        <div className={styles.arrowContainer}>
           <ArrowDown />
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
